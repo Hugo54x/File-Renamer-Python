4e5a5ecce811e420b7757504d05fd2b34bb84e1d
Pierre <10262302+Hugo54x@users.noreply.github.com>
2019-11-22 00:03:32 +0100

    Updated renaming to a more generic way

--- FileRenamer.py
+++ FileRenamer.py
@@ -4,13 +4,12 @@ from shutil import copyfile
 def main():
     j = 0
     for filename in os.listdir('./Input/'):
-
         src = "./Input/" + filename
         dst = "./Output/" + filename
         copyfile(src, dst)
-        src = "./Output/" + filename
-        dst = src + "_" + str(j).zfill(4) + ".jpg"
-        os.rename(src, dst)
+        oldStr = "./Output/" + filename
+        newStr = "./Output/" + str(j).zfill(4) + ".jpg"
+        os.rename(oldStr, newStr)
         j += 1
 
 if __name__ == '__main__':
